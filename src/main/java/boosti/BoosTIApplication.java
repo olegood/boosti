45f52f17330e3260ceef016e5965aab5be0e8dfb
Oleg Anastassov <oleg.anastassov@gmail.com>
2022-01-26 14:48:20 +0300

    #1 Initial project structure
    - create initial structure and directories layout
    
    Closes #1

---
+++ src/main/java/boosti/BoosTIApplication.java
@@ -0,0 +1,12 @@
+package boosti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoosTIApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BoosTIApplication.class, args);
+  }
+}
---
+++ src/test/java/boosti/BoosTIApplicationTests.java
@@ -0,0 +1,11 @@
+package boosti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoosTIApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
