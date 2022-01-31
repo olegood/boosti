70237ae5ed1733cbd6c2d8a96dcd2e2925095e67
Oleg Anastassov <oleg.anastassov@gmail.com>
2022-01-31 21:52:55 +0300

    ## Add JS test for Header component

--- client/src/components/common/header/__tests__/header.test.js
+++ client/src/components/common/header/__tests__/header.test.js
@@ -1,8 +1,29 @@
-import Header from "../header";
-import {render} from '@testing-library/react'
+import Header from '../header'
+import renderer from 'react-test-renderer'
+import {Typography} from "@mui/material";
 
-test.skip('header should have text', () => {
-  const {getByRole, debug} = render(<Header text={'This is a header'}/>)
-  //expect(result).toContain(/This is a header/i)
-  fail('Not implemented yet.')
+describe('<Header />', () => {
+  test('snapshot', () => {
+    // when
+    const component = renderer.create(<Header text={'Header text'}/>)
+
+    // then
+    const tree = component.toJSON()
+    expect(tree).toMatchSnapshot();
+  })
+
+  test('should render correctly', () => {
+    // given
+    const text = 'Header text'
+
+    // when
+    const testRenderer = renderer.create(<Header text={text}/>)
+    const testInstance = testRenderer.root
+
+    // then
+    expect(testInstance.findByType(Typography).props.variant).toBe('h3');
+    expect(testInstance.findByType(Typography).props.component).toBe('div');
+
+    expect(testInstance.findByType(Typography).props.children).toEqual(text)
+  })
 })
--- client/src/components/common/header/header.js
+++ client/src/components/common/header/header.js
@@ -5,11 +5,10 @@ import PropTypes from "prop-types";
 export default function Header(props) {
   const {text} = props
   return (<>
-      <Typography variant="h3" component="div">
-        {text}
-      </Typography>
-    </>
-  )
+    <Typography variant="h3" component="div">
+      {text}
+    </Typography>
+  </>)
 }
 
 Header.propTypes = {
