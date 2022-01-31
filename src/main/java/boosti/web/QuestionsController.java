00ba64d05a83e10c6724f2a7561e4daebb9f4c07
Oleg Anastassov <oleg.anastassov@gmail.com>
2022-01-31 16:49:47 +0300

    #17 Upload CSV file through UI
    Close #17

--- src/main/java/boosti/web/QuestionsController.java
+++ src/main/java/boosti/web/QuestionsController.java
@@ -2,13 +2,13 @@ package boosti.web;
 
 import static org.springframework.util.CollectionUtils.isEmpty;
 
-import boosti.model.Question;
-import boosti.service.QuestionsService;
-import boosti.service.export.ExportService;
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
+
+import boosti.model.Question;
+import boosti.service.QuestionsService;
+import boosti.service.export.ExportService;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -36,6 +36,17 @@ public class QuestionsController {
   public ResponseEntity<?> saveQuestion(@RequestBody Question question) {
     var result = questionsService.save(question);
     return ResponseEntity.status(HttpStatus.CREATED).body(result);
+  }
+
+  @PostMapping("/api/questions/export")
+  public ResponseEntity<Resource> export(@RequestBody Set<Long> ids) {
+    var questions = questionsService.getById(ids);
+    if (isEmpty(questions)) {
+      return ResponseEntity.noContent().build();
+    }
+
+    var resource = exportService.export(questions);
+    return ResponseEntity.ok().contentType(MediaType.APPLICATION_OCTET_STREAM).body(resource);
   }
 
   @GetMapping("/api/questions")
