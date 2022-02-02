9d60f82244f6659ce2ebc84a4240c29f5108aee7
Oleg Anastassov <oleg.anastassov@gmail.com>
2022-02-02 08:13:17 +0300

    #11 Global CORS Configuration
    Close #11

--- src/main/java/boosti/web/FileController.java
+++ src/main/java/boosti/web/FileController.java
@@ -8,13 +8,11 @@ import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 import java.util.function.Predicate;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class FileController {
 
--- src/main/java/boosti/web/QuestionsController.java
+++ src/main/java/boosti/web/QuestionsController.java
@@ -12,14 +12,12 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class QuestionsController {
 
