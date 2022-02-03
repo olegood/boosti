e4c50a1e6cdf72ffd5e5b4316659c5cbbd39557f
Oleg Anastassov <oleg.anastassov@gmail.com>
2022-02-03 18:27:25 +0300

    ## Properly cover Questions equality by unit tests

--- src/test/java/boosti/model/QuestionTest.java
+++ src/test/java/boosti/model/QuestionTest.java
@@ -2,38 +2,40 @@ package boosti.model;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+import java.util.stream.Stream;
 
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class QuestionTest {
 
-  @Test
-  void twoQuestionsShouldBeEqualIfTopicAndTextAreTheSame() {
-    // given
-    var one = new Question(42L, "<topic>", "<text>");
-    var two = new Question(58L, "<topic>", "<text>");
-
+  @ParameterizedTest
+  @MethodSource("questionsEqualityProvider")
+  void checkTwoQuestionsAreEqual(Question one, Object two, boolean expected) {
     // when
     var result = one.equals(two);
 
     // then
-    assertThat(result, Matchers.is(true));
+    assertThat(result, Matchers.is(expected));
   }
 
-  @Test
-  void twoQuestionsShouldNoBeEqualIfTopicAndTextDiffer() {
-    // given
-    var one = new Question(42L, "<topic One>", "<text One>");
-    var two = new Question(58L, "<topic>", "<text>");
+  static Stream<Arguments> questionsEqualityProvider() {
+    return Stream.of(
+        arguments(from(42L, "<topic>", "<text>"), from(58L, "<topic>", "<text>"), true),
+        arguments(from(42L, "<topic One>", "<text One>"), from(58L, "<topic>", "<text>"), false),
+        arguments(from(42L, "<topic>", "<text 1>"), from(58L, "<topic>", "<text 2>"), false),
+        arguments(from(42L, "<topic 1>", "<text>"), from(58L, "<topic 2>", "<text>"), false),
+        arguments(from(42L, "<topic One>", "<text One>"), new Object(), false));
+  }
 
-    // when
-    var result = one.equals(two);
-
-    // then
-    assertFalse(result);
+  private static Question from(Long id, String topic, String text) {
+    return new Question(id, topic, text);
   }
 
   @Test
@@ -51,7 +53,7 @@ class QuestionTest {
   }
 
   @Test
-  void twoQuestionsShouldNotHaveTheSameHashIfTheyAreEqual() {
+  void twoQuestionsShouldNotHaveTheSameHashIfTheyAreNotEqual() {
     // given
     var one = new Question(42L, "<topic>", "<text>");
     var two = new Question(58L, "<topic Two>", "<text Two>");
