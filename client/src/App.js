83377e28d9c68309e2ad134cf7179323c27de32b
Oleg Anastassov <oleg.anastassov@gmail.com>
2022-02-07 11:18:13 +0300

    ## Extract routes used as hardcoded values

--- client/src/App.js
+++ client/src/App.js
@@ -4,24 +4,43 @@ import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 import React from 'react'
 import { Route, Routes } from 'react-router'
+import PageAbout from './content/about/pageAbout.js'
 import AppFooter from './content/appFooter.js'
 import Welcome from './content/home/welcome.js'
 import ImportContainer from './content/import/importContainer.js'
-import PageAbout from './content/about/pageAbout.js'
 import AddQuestion from './content/questions/addQuestion.js'
 import QuestionsGrid from './content/questions/questionsGrid.js'
 import AppNavigation from './navigation/appNavigation.js'
+
+const routes = [
+  {
+    path: '/',
+    element: <Welcome/>
+  },
+  {
+    path: '/questions',
+    element: <QuestionsGrid/>
+  },
+  {
+    path: '/questions/add',
+    element: <AddQuestion/>
+  },
+  {
+    path: '/import',
+    element: <ImportContainer/>
+  },
+  {
+    path: '/about',
+    element: <PageAbout/>
+  }
+]
 
 export default function App() {
   return (
     <React.Fragment>
       <AppNavigation/>
       <Routes>
-        <Route path="/" element={<Welcome/>}/>
-        <Route path="/questions" element={<QuestionsGrid/>}/>
-        <Route path="/questions/add" element={<AddQuestion/>}/>
-        <Route path="/import" element={<ImportContainer/>}/>
-        <Route path="/about" element={<PageAbout/>}/>
+        {routes?.map(({ path, element }) => <Route path={path} element={element}/>)}
       </Routes>
       <AppFooter/>
     </React.Fragment>
