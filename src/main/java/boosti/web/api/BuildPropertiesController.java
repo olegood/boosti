9bd4d970e0fbcb2de3b671ae60996118faf04c38
Oleg Anastassov <oleg.anastassov@gmail.com>
2022-02-11 22:49:11 +0300

    ## Remove unnecessary @Autowired annotation

--- src/main/java/boosti/web/api/BuildPropertiesController.java
+++ src/main/java/boosti/web/api/BuildPropertiesController.java
@@ -2,22 +2,22 @@ package boosti.web.api;
 
 import java.util.Properties;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/build-properties")
 public class BuildPropertiesController {
 
   private final BuildProperties build;
 
-  @Autowired
   public BuildPropertiesController(BuildProperties build) {
     this.build = build;
   }
 
-  @GetMapping("/api/build-properties")
+  @GetMapping
   public Properties getBuildInfo() {
     final var props = new Properties();
     build.forEach(entry -> props.putIfAbsent(entry.getKey(), entry.getValue()));
