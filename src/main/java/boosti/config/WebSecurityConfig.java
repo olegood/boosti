27617be00cf6fe6af951ac5f24074ccd40e290d6
Oleg Anastassov <oleg.anastassov@gmail.com>
2022-03-05 16:25:12 +0300

    ## CSRF ignore for API pattern

--- src/main/java/boosti/config/WebSecurityConfig.java
+++ src/main/java/boosti/config/WebSecurityConfig.java
@@ -45,7 +45,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.csrf()
-        .disable()
+        .ignoringAntMatchers("/api/**")
+        .and()
         .authorizeRequests()
         .antMatchers("/api/**")
         .fullyAuthenticated()
