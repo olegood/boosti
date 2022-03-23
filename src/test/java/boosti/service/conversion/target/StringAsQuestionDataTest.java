e863c1c3f53796aa336b890367c3ae55f90efa3a
Oleg Anastassov <oleg.anastassov@gmail.com>
2022-03-23 18:12:29 +0300

    ## Resolve 'todo' comments

--- src/test/java/boosti/service/conversion/target/StringAsQuestionDataTest.java
+++ src/test/java/boosti/service/conversion/target/StringAsQuestionDataTest.java
@@ -13,7 +13,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-// todo: check tests names
 class StringAsQuestionDataTest {
 
   @Test
