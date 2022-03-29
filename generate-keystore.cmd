48e234de515e1f0a6d4fb43eb3d46ba5c11c292d
Oleg Anastassov <oleg.anastassov@gmail.com>
2022-03-29 11:18:49 +0300

    #52 Tools: Move CLI script to generate keystore
    - move generate-keystore.cmd to tools directory
    - change the target path in file accordingly
    
    Close #52

--- generate-keystore.cmd
+++
@@ -1,1 +0,0 @@
-keytool -genkeypair -alias boosti -keyalg RSA -keysize 2048 -storetype PKCS12 -keystore src/main/resources/keystore/root.p12 -validity 3650
---
+++ tools/generate-keystore.cmd
@@ -0,0 +1,3 @@
+keytool -genkeypair -alias boosti -keyalg RSA -keysize 2048 -storetype PKCS12 \
+-keystore ../src/main/resources/keystore/root.p12 \
+-validity 3650
