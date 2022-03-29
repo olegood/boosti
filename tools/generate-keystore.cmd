924442b1e51786c402d07906b032879d7b01e164
Oleg Anastassov <oleg.anastassov@gmail.com>
2022-03-29 16:29:49 +0300

    #52 Tools: Move CLI script to generate keystore
    - fix new line issue while running script

--- tools/generate-keystore.cmd
+++ tools/generate-keystore.cmd
@@ -1,3 +1,1 @@
-keytool -genkeypair -alias boosti -keyalg RSA -keysize 2048 -storetype PKCS12 \
--keystore ../src/main/resources/keystore/root.p12 \
--validity 3650
+keytool -genkeypair -alias boosti -keyalg RSA -keysize 2048 -storetype PKCS12 -keystore ../src/main/resources/keystore/root.p12 -validity 3650
